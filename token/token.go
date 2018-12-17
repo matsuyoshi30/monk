f13df011d41420684af063bf55f8318e5dddbf51
matsuyoshi <sfbgwm30@gmail.com>
2018-12-18 00:36:23 +0900

    Initial commit.

---
+++ token/token.go
@@ -0,0 +1,68 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF" // end of file
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y ...
+	INT   = "INT"   // 5, 10 ...
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+	MULTIPLY = "*"
+	DIVIDE = "/"
+
+	GT = ">"
+	LT = "<"
+
+	NOT = "!"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+}
+
+// LookupIdent ...
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
