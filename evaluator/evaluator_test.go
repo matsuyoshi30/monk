eaa48a976e1bfceea27dac2d03a1e4a09740193f
matsuyoshi <sfbgwm30@gmail.com>
2019-01-09 00:12:50 +0900

    add test about closure

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -303,3 +303,16 @@ func TestFunctionApplicatio(t *testing.T) {
 		testIntegerObject(t, evaluated, tt.expected)
 	}
 }
+
+func TestClosures(t *testing.T) {
+	input := `
+let newAdder = fn(x) {
+  fn(y) { x + y; }
+};
+
+let addTwo = newAdder(2);
+addTwo(2);
+`
+
+	testIntegerObject(t, testEval(input), 4)
+}
